d895f377a2c0e9cee8e2ee32158d24ff7bb25a77
Chae-young Park <pcy07111@gmail.com>
2023-07-25 19:21:25 +0900

    docs: Create a new project

---
+++ calculator-app/calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by 박채영 on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
